3a607be2258bd73ff4d0fa9956a63c6963bdb7e4
meerasahib <31014549+meerasahib@users.noreply.github.com>
2017-08-26 14:53:46 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,127 +1,57 @@
-import React, { Component } from 'react';
-import Link from 'gatsby-link';
-import styled from 'styled-components';
-
-import particlesConfig from '../json/particles-config.json';
-
-import '../css/particle-styles.css';
-import { animateBackground, animateShake } from '../css/animations';
-
-const Header = styled.header`
-  ${animateBackground} ${animateShake} height: ${props =>
-      props.isPost ? '15vh' : '25vh'};
-  background-color: #ffa81f;
-  color: blue;
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(#ffa81f, #d85d15);
-  background-size: 250% 250%;
-  animation: animateBg 10s ease infinite;
-  font-weight: 400;
-  transition: height 250ms ease-in-out;
-  user-select: none;
-  @media only screen and (min-width: 768px) {
-    height: ${props => (props.isPost ? '30vh' : '45vh')};
-  }
-`;
-
-const Name = styled.h1`
-  display: flex;
-  flex-wrap: wrap;
-  font-size: 2rem;
-  text-transform: uppercase;
-  font-weight: 700;
-  align-items: center;
-  transition: font-size 250ms ease-in-out, padding 150ms ease-in;
-  background-color: #002635;
-  color: white;
-  padding: 0.5rem 1rem;
-  margin: 0;
-  width: auto;
-  user-select: text;
-  .wf-active & {
-    font-family: 'Montserrat', sans-serif;
-  }
-  @media only screen and (min-width: 375px) {
-    font-size: 2.5rem;
-  }
-  @media only screen and (min-width: 768px) {
-    font-size: 4.5rem;
-    padding: 1rem 2rem;
-  }
-`;
-
-const Letter = styled.span`
-  display: inline-block;
-  position: relative;
-  z-index: 3;
-  &:hover {
-    animation: shake 1000ms ease-in-out;
-  }
-`;
-
-const First = styled.span`
-  padding-right: 2vw;
-  font-weight: 700;
-  white-space: nowrap;
-`;
-
-const Last = styled.span`
-  font-weight: 400;
-  white-space: nowrap;
-`;
-
-const NoWrap = styled.span`white-space: nowrap;`;
-
-const StyledLink = styled(Link)`
-  color: inherit;
-`;
-
-class BlogHeader extends Component {
-  componentDidMount() {
-    if (window) {
-      require.ensure('particles.js', () => {
-        this.Particles = require('particles.js');
-        this.Particles(`blog-header`, particlesConfig);
-      }, 'particles.js');
+module.exports = {
+  pathPrefix: '/blog',
+  siteMetadata: {
+    title: `Meera Sahib - Blog`,
+    author: `Mohamed Meera Sahib`,
+  },
+  plugins: [
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          'gatsby-remark-copy-linked-files',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              linkImagesToOriginal: false
+            }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-autolink-headers'
+        ]
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-105102373-1'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `Dustin Schau's Blog`,
+        description: 'The blog of the developer, Dustin Schau',
+        short_name: 'DSchau Blog',
+        background_color: 'white',
+        theme_color: '#002635',
+        orientation: 'portrait',
+        display: 'minimal-ui'
+      }
     }
-  }
-
-  componentWillUnmount() {
-    if (this.Particles) {
-      this.Particles.destroy();
-    }
-  }
-
-  render() {
-    return (
-      <Header id="blog-header" {...this.props}>
-        <Name className="name">
-          <StyledLink to="/">
-            <First>
-              {'Dustin'.split('').map((letter, index) =>
-                <Letter key={`${letter}-${index}`}>
-                  {letter}
-                </Letter>
-              )}
-            </First>
-            <Last>
-              {'Schau'.split('').map((letter, index) =>
-                <Letter key={`${letter}-${index}`}>
-                  {letter}
-                </Letter>
-              )}
-            </Last>
-          </StyledLink>
-        </Name>
-      </Header>
-    );
-  }
+  ],
 }
-
-export default BlogHeader;
